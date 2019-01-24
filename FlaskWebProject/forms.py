ea804456f68b1b6ab16488d7be8327915fec47ce
Waran Arulselvam <Waran.Arulselvam@pret.com>
2019-01-24 22:56:34 +0000

    22

--- FlaskWebProject/forms.py
+++ FlaskWebProject/forms.py
@@ -5,7 +5,7 @@ from wtforms import Form, TextField, TextAreaField, validators, StringField, Sub
 from wtforms.fields.html5 import DateField
 from FlaskWebProject import data
 
-shops = [(30, 'Marble Archxxx'), (10472, '1Stansted Airportyyyyyy')]
+shops = [(30, 'Marble ArchMMM'), (10472, '1Stansted Airportyyyyyy')]
 #shops = data.get_shops(1)
 
 class BenchForm(Form):
